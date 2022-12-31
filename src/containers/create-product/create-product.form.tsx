014a333fe75fca6e46d07bc5b29474adcc2d6d78
ePaulo <paul.olynek@live.ca>
2022-12-31 10:39:05 -0800

    Added code for dynamically adding/removing color info for a product.

--- src/containers/create-product/create-product.form.tsx
+++ src/containers/create-product/create-product.form.tsx
@@ -1,10 +1,11 @@
 import './create-product.styles.scss'
-import { useForm } from 'react-hook-form'
+import { Fragment } from 'react'
+import { Control, useFieldArray, useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as z from 'zod'
 
-const DataSchema = z.object({
-  id: z.string().uuid({ message: 'Invalid ID' }),
+const ProductSchema = z.object({
+  pId: z.string().uuid({ message: 'Invalid ID' }),
 
   name: z
     .string()
@@ -22,83 +23,161 @@ const DataSchema = z.object({
       message: 'Invalid characters in the description',
     }),
 
-  price: z
-    .number({ invalid_type_error: 'Enter dollar value, numbers only' })
-    .gte(0, { message: 'Enter a positive value (0 if FREE)' })
-    .lte(10000, { message: 'Price must be less than or equal to $10,000' })
-    .refine(value => (value * 100) % 1 === 0, {
-      message: 'Price must be a multiple of 0.01',
+  colors: z.array(
+    z.object({
+      cId: z.string().uuid({ message: 'Invalid ID' }),
+
+      color: z.string().regex(/^[A-Za-z0-9 -_:]+$/, {
+        message: 'Invalid characters in the color',
+      }),
+
+      price: z
+        .number({ invalid_type_error: 'Enter dollar value, numbers only' })
+        .gte(0, { message: 'Enter a positive value (0 if FREE)' })
+        .lte(10000, { message: 'Price must be less than or equal to $10,000' })
+        .refine(value => (value * 100) % 1 === 0, {
+          message: 'Price must be a multiple of 0.01',
+        }),
+
+      imageURL: z.string().regex(/\.(gif|jpe?g|tiff?|png|bmp)$/i, {
+        message: 'Enter image file name (gif/jpg/png/bmp) or URL',
+      }),
+
+      quantity: z
+        .number({ invalid_type_error: 'Enter a whole number' })
+        .int({ message: 'Quantity must be a whole number' })
+        .gte(0, { message: 'Quantity must be a positive number' })
+        .lte(10000, { message: 'Current max-quantity is 10,000 units' }),
     })
-    .default(0),
-
-  imageURL: z.string().regex(/\.(gif|jpe?g|tiff?|png|bmp)$/i, {
-    message: 'Enter image file name (gif/jpg/png/bmp) or URL',
-  }),
-
-  quantity: z
-    .number({ invalid_type_error: 'Enter a whole number' })
-    .int({ message: 'Quantity must be a whole number' })
-    .gte(0, { message: 'Quantity must be a positive number' })
-    .lte(10000, { message: 'Current max-quantity is 10,000 units' })
-    .default(0),
+  ),
 })
 
-type FormData = z.infer<typeof DataSchema>
+type FormDataType = z.infer<typeof ProductSchema>
 
 const CreateProductForm = () => {
   const {
+    control,
     register,
     reset,
     setValue,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormData>({
-    resolver: zodResolver(DataSchema),
+  } = useForm<FormDataType>({
+    resolver: zodResolver(ProductSchema),
     shouldUseNativeValidation: false,
+    defaultValues: {
+      pId: 'b12fcc08-b480-46da-9373-10a91a045627',
+      name: 'Product One',
+      description: 'Description for Product One',
+      colors: [
+        {
+          cId: 'f7d83629-2b86-4250-b22e-db879ec2c2c7',
+          color: 'Yellow',
+          price: 11.99,
+          imageURL: 'srcassets\\1__dBwadhWa-lI-unsplash.jpg',
+          quantity: 101,
+        },
+      ],
+    },
   })
 
-  const onSubmit = (data: FormData) => {
-    // console.log(errors)
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: 'colors',
+    keyName: 'cId',
+  })
+
+  const onSubmit = (data: FormDataType) => {
+    console.log('Attempt to submit Create Product Form data')
+    console.log(errors)
     console.log(data)
     reset()
   }
 
   return (
     <form className='create-product__form' onSubmit={handleSubmit(onSubmit)}>
-      {/* <label htmlFor='id'>
-        ID:
-        {errors.id && <span>{errors.id.message}</span>}
-      </label>
-      <input id='id' {...register('id')} /> */}
       <label htmlFor='name'>
         Name:
-        {errors.name && <span>{errors.name.message}</span>}
+        <span>{errors.name?.message}</span>
       </label>
       <input id='name' {...register('name')} />
+
       <label htmlFor='description'>
         Description:
-        {errors.description && <span>{errors.description.message}</span>}
+        <span>{errors.description?.message}</span>
       </label>
       <textarea id='description' {...register('description')} />
-      <label htmlFor='price'>
-        Price:
-        {errors.price && <span>{errors.price.message}</span>}
-      </label>
-      <input id='price' {...register('price', { valueAsNumber: true })} />
-      <label htmlFor='imageURL'>
-        Image URL:
-        {errors.imageURL && <span>{errors.imageURL.message}</span>}
-      </label>
-      <input id='imageURL' {...register('imageURL')} />
-      <label htmlFor='quantity'>
-        Quantity:
-        {errors.quantity && <span>{errors.quantity.message}</span>}
-      </label>
-      <input id='quantity' {...register('quantity', { valueAsNumber: true })} />
 
-      <button type='submit' onClick={() => setValue('id', crypto.randomUUID())}>
+      {fields.map((colorField, index) => (
+        <Fragment key={colorField.cId}>
+          <label htmlFor={`colors[${index}].color`}>
+            Color:
+            <span>{errors.colors?.[index]?.color?.message}</span>
+          </label>
+          <input
+            id={`colors[${index}].color`}
+            {...register(`colors.${index}.color` as const)}
+          />
+
+          <label htmlFor={`colors[${index}].price`}>
+            Price:
+            <span>{errors.colors?.[index]?.price?.message}</span>
+          </label>
+          <input
+            id={`colors[${index}].price`}
+            {...register(`colors.${index}.price` as const, {
+              valueAsNumber: true,
+            })}
+          />
+
+          <label htmlFor={`colors[${index}].imageURL`}>
+            Image URL:
+            <span>{errors.colors?.[index]?.imageURL?.message}</span>
+          </label>
+          <input
+            id={`colors[${index}].imageURL`}
+            {...register(`colors.${index}.imageURL` as const)}
+          />
+
+          <label htmlFor={`colors[${index}].quantity`}>
+            Quantity:
+            <span>{errors.colors?.[index]?.quantity?.message}</span>
+          </label>
+          <input
+            id={`colors[${index}].quantity`}
+            {...register(`colors.${index}.quantity` as const, {
+              valueAsNumber: true,
+            })}
+          />
+
+          <button type='button' onClick={() => remove(index)}>
+            Remove
+          </button>
+        </Fragment>
+      ))}
+      <button
+        type='button'
+        onClick={() =>
+          append({
+            cId: crypto.randomUUID(),
+            color: '',
+            price: 0,
+            imageURL: '',
+            quantity: 0,
+          })
+        }
+      >
+        Add Color
+      </button>
+
+      <button
+        type='submit'
+        onClick={() => setValue('pId', crypto.randomUUID())}
+      >
         Submit
       </button>
+
+      <button type='reset'>Reset</button>
     </form>
   )
 }
